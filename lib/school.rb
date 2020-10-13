2c7ca14d93cdf047e6c195e5c7cbc06227e0f0a8
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-13 00:24:30 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School 
+    attr_reader :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student (student_name, grade_level)
+        if !@roster[grade_level]
+            @roster[grade_level] = []
+        end  
+        @roster[grade_level] << student_name
+        
+    end
+
+    def grade(grade_level)
+        @roster[grade_level]
+    end
+    def sort 
+        # @roster.sort_by {|key, value| key}.to_h
+        @roster.sort_by{|key, value| key}.map do |grade|
+            [grade[0], grade[1].sort]
+        end.to_h
+    end
+end
